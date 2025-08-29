3f09317a56d1ad232c6aa80c7db993a6a8f51c31
Louis Falk Knudsen <louis@falkknudsen.com>
2025-08-29 12:01:18 +0200

    First iteration of printing the given CSV file to the standard output. File hard-coded.
    Co-authored-by: My <mmga@itu.dk>
    Co-authored-by: Nikki <nris@itu.dk>
    Co-authored-by: Kris <krme@itu.dk>
    Co-authored-by: Hassan <halw@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+namespace Chirp {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chirps = File.ReadAllLines("chirp_cli_db.csv");
+            for (int i = 1; i < chirps.Length; i++)
+            {
+                int indexOfFirstComma = chirps[i].IndexOf(",");
+                int indexOfLastComma = chirps[i].LastIndexOf(",");
+                string author = chirps[i].Substring(0, indexOfFirstComma);
+                string message = chirps[i].Substring(indexOfFirstComma + 2, indexOfLastComma - 7);
+                string timestamp = chirps[i].Substring(indexOfLastComma + 1);
+
+                DateTime time = new DateTime(1970, 1, 1);
+                time = time.AddSeconds(int.Parse(timestamp));
+                
+                Console.WriteLine(author + " @ " + time + ": " + message);
+            }
+        }
+    }
+}
