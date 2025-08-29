b37bee30979606e8c81182b0e79f85f1c87f02c8
Louis Falk Knudsen <louis@falkknudsen.com>
2025-08-29 12:09:26 +0200

    Only prints something when "read" argument is added.
    Co-authored-by: My <mmga@itu.dk>
    Co-authored-by: Nikki <nris@itu.dk>
    Co-authored-by: Kris <krme@itu.dk>
    Co-authored-by: Hassan <halw@itu.dk>

--- Program.cs
+++ Program.cs
@@ -5,19 +5,22 @@ namespace Chirp {
     {
         static void Main(string[] args)
         {
-            var chirps = File.ReadAllLines("chirp_cli_db.csv");
-            for (int i = 1; i < chirps.Length; i++)
+            if (args.Length > 0 && args[0] == "read")
             {
-                int indexOfFirstComma = chirps[i].IndexOf(",");
-                int indexOfLastComma = chirps[i].LastIndexOf(",");
-                string author = chirps[i].Substring(0, indexOfFirstComma);
-                string message = chirps[i].Substring(indexOfFirstComma + 2, indexOfLastComma - 7);
-                string timestamp = chirps[i].Substring(indexOfLastComma + 1);
-
-                DateTime time = new DateTime(1970, 1, 1);
-                time = time.AddSeconds(int.Parse(timestamp));
+                string[] chirps = File.ReadAllLines("chirp_cli_db.csv");
+                for (int i = 1; i < chirps.Length; i++)
+                {
+                    int indexOfFirstComma = chirps[i].IndexOf(",");
+                    int indexOfLastComma = chirps[i].LastIndexOf(",");
+                    string author = chirps[i].Substring(0, indexOfFirstComma);
+                    string message = chirps[i].Substring(indexOfFirstComma + 2, indexOfLastComma - 7);
+                    string timestamp = chirps[i].Substring(indexOfLastComma + 1);
+    
+                    DateTime time = new DateTime(1970, 1, 1);
+                    time = time.AddSeconds(int.Parse(timestamp));
                 
-                Console.WriteLine(author + " @ " + time + ": " + message);
+                    Console.WriteLine(author + " @ " + time + ": " + message);
+                }
             }
         }
     }
