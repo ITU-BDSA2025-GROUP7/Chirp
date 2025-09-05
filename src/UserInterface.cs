bcecce7d2875e349b0d85a498eb3f4f99112b09e
My <mmga@itu.dk>
2025-09-05 13:23:39 +0200

    Seperated code concerning printing cheeps to a userinterface class

---
+++ src/UserInterface.cs
@@ -0,0 +1,12 @@
+namespace Chirp;
+
+public static class UserInterface
+{
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (var cheep in cheeps)
+        {
+            Console.WriteLine(cheep);
+        }
+    }
+}
