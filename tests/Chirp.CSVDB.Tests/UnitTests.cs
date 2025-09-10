854d7ab505dd45e62514c8e3e6e02a5551e9e032
Louis Falk Knudsen <louis@falkknudsen.com>
2025-09-10 17:00:20 +0200

    Now testing that CsvDataBase.Read() returns the expected number of results.
    Co-authored-by: Hassan <halw@itu.dk>

---
+++ tests/Chirp.CSVDB.Tests/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Chirp.CLI.Client;
+using System.IO;
+
+namespace Chirp.CSVDB.Tests;
+
+public class UnitTests
+{
+	[Fact]
+    public void Read()
+    {
+		string current = Directory.GetCurrentDirectory(); // net9.0
+		DirectoryInfo dir = new DirectoryInfo(current);
+		// Must check that .Parent property is not null or you get warnings.
+		Assert.NotNull(dir);
+		Assert.NotNull(dir.Parent);
+		Assert.NotNull(dir.Parent.Parent);
+		Assert.NotNull(dir.Parent.Parent.Parent);
+		dir = dir.Parent.Parent.Parent;
+		string filename = "chirp_cli_db.csv";
+		string dirString = dir.FullName;
+		string path = Path.Combine(dirString, filename);
+		Assert.True(File.Exists(path));
+
+		CsvDataBase<Cheep> database = new CsvDataBase<Cheep>(path);
+		List<Cheep> results = database.Read().ToList();
+		Assert.Equal(8, results.Count);
+    }
+}
