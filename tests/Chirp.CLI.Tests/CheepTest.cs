7f823ce3bc38d17f24d5e9ffa79e8de953561ab6
Nikki <nikki.s.risager@gmail.com>
2025-09-13 11:25:38 +0200

    Fixed a bug where the test "cheepToString" would fail depending on what timezone you are on. Also added some user settings to the gitignore

--- tests/Chirp.CLI.Tests/CheepTest.cs
+++ tests/Chirp.CLI.Tests/CheepTest.cs
@@ -22,20 +22,22 @@ public class CheepTest
     [Theory]
     [InlineData(1690891760, "01-08-2023 14:09:20")]
     [InlineData(-1690891760, "02-06-1916 13:50:40")]
-    public void cheepToString(int timestamp, string expectedConversion)
+    public void cheepToString(int timestamp, string expectedTimeString)
     {
         //arrange
         string Author = "Chirp";
         string Message = "Hello World, i am alive!!!!";
         long Timestamp = timestamp; // this tecnically also tests time conversion
-
-        string expected ="Chirp @ " + expectedConversion + ": Hello World, i am alive!!!!";
+        // Calculates the expeted time since it will depend on what time zone the computer running the test will be in
+        String ExpectedTime = DateTime.Parse(expectedTimeString + " +02:00").ToLocalTime().ToString("dd-MM-yyyy HH:mm:ss"); 
+        
+        string expected ="Chirp @ " + ExpectedTime + ": Hello World, i am alive!!!!";
+        
         //act
         Cheep cheep = new Cheep(Author, Message, Timestamp);
-
         string result = cheep.ToString();
+        
         //assert
         Assert.Equal(expected, result);
-
     }
 }
