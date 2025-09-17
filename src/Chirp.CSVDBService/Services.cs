a5fd39b3ee9bd10f494be0c25531e0cc68e960cd
Nikki <nikki.s.risager@gmail.com>
2025-09-17 15:27:35 +0200

    Added projects

---
+++ src/Chirp.CSVDBService/Services.cs
@@ -0,0 +1,16 @@
+using Chirp.CSVDB;
+
+var db = Chirp.CSVDB.CsvDataBase<>
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+
+app.MapGet("/cheeps", () =>
+{
+    
+    
+})
+
+app.Run();
