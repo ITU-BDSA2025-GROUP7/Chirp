8440935e996811d6d84756b7c978d3ec3587c43e
Louis <louis@falkknudsen.com>
2025-09-25 22:01:33 +0200

    Created a project for the database facade, added SQLite package dependency, and embedded the example data resources

---
+++ src/DBFacade/DBFacade.cs
@@ -0,0 +1,15 @@
+﻿using Chirp.CSVDB;
+
+namespace DBFacade;
+
+public class DBFacade<T> : IDataBaseRepository<T> {
+    private string _dbLocation = Environment.GetEnvironmentVariable("CHIRPDBPATH")!;
+    
+    public IEnumerable<T> Read(int? limit = null) {
+        throw new NotImplementedException();
+    }
+
+    public void Store(T record) {
+        throw new NotImplementedException();
+    }
+}
