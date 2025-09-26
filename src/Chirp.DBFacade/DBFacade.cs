3b3785d21fa63bc26db5a134083b614ee70610cd
Louis <louis@falkknudsen.com>
2025-09-26 14:46:13 +0200

    Reduced duplicate code, and added the read-limit to Read(int?). The query strings are now constants defined at beginning of class for easier maintenance.

--- src/Chirp.DBFacade/DBFacade.cs
+++ src/Chirp.DBFacade/DBFacade.cs
@@ -15,6 +15,11 @@ public class DBFacade<T> : IDataBaseRepository<T> {
     private readonly string _dbLocation = 
         Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? "";
     
+    private const string READ_ALL_QUERY = 
+        "SELECT (author_id, pub_data, message) FROM messages ORDER BY pub_data desc";
+    private const string READ_SOME_QUERY = 
+        "SELECT (author_id, pub_data, message) FROM messages ORDER BY pub_data desc LIMIT @limit";
+    
     private static DBFacade<T>? _instance = null; //  ? makes it nullable
     private static readonly Lock _padlock = new Lock();
     
@@ -30,27 +35,10 @@ public class DBFacade<T> : IDataBaseRepository<T> {
     }
     
 
-    /** Load the example data into the database. */
-    private static SqliteConnection LoadData(SqliteConnection connection) {
+    /** Execute the given embedded file's SQL command, e.g. inserting schema or initial data. */
+    private static SqliteConnection ExecuteCommandInFile(SqliteConnection connection, string filename) {
         var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
-        using Stream? reader = embeddedProvider.GetFileInfo(INITIAL_DATA_PATH).CreateReadStream();
-        using var sr = new StreamReader(reader);
-        string query = sr.ReadToEnd();
-        
-        SqliteTransaction transaction = connection.BeginTransaction();
-        SqliteCommand command = connection.CreateCommand();
-        command.Transaction = transaction;
-        command.CommandText = query;
-        command.ExecuteNonQuery();
-        transaction.Commit();
-        
-        return connection;
-    }
-    
-    /** Load the database schema into the database. */
-    private static SqliteConnection LoadSchema(SqliteConnection connection) {
-        var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
-        using Stream? reader = embeddedProvider.GetFileInfo(SCHEMA_PATH).CreateReadStream();
+        using Stream? reader = embeddedProvider.GetFileInfo(filename).CreateReadStream();
         using var sr = new StreamReader(reader);
         string query = sr.ReadToEnd();
         
@@ -75,22 +63,30 @@ public class DBFacade<T> : IDataBaseRepository<T> {
         string dbFile = Path.Join(tempDir, DEFAULT_DATABASE_FILENAME);
         var connection = new SqliteConnection($"Data Source={dbFile}");
         connection.Open();
-        LoadSchema(connection);
-        LoadData(connection);
+        ExecuteCommandInFile(connection, SCHEMA_PATH);
+        ExecuteCommandInFile(connection, INITIAL_DATA_PATH);
         return connection;
     }
     
-    /** Read up to <c>limit</c> records from the database. */
     public IEnumerable<T> Read(int? limit = null) {
         using SqliteConnection connection = Connect();
         connection.Open();
         SqliteCommand command = connection.CreateCommand();
-        command.CommandText = "SELECT (author_id, pub_data, message) FROM messages ORDER BY pub_data desc LIMIT @limit";
+        
+        if (limit.HasValue) {
+            command.CommandText = READ_SOME_QUERY;
+        } else {
+            command.CommandText = READ_ALL_QUERY;
+        }
+        command.Transaction = connection.BeginTransaction();
         using SqliteDataReader reader = command.ExecuteReader();
         var data = new List<T>(limit ?? 10);
         while (reader.Read()) {
             data.AddRange(reader.Cast<T>());
         }
+        command.Transaction.Commit();
+        reader.Close();
+        connection.Close();
         return data;
     }
 
