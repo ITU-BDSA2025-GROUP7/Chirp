0c182896e607a3d8f222af0b105e413cde438dd1
Louis <louis@falkknudsen.com>
2025-09-27 02:42:35 +0200

    Implemented storing to the database. Still entirely decoupled from the Cheep class, though admittedly it does make the code more complex.

--- src/Chirp.DBFacade/DBFacade.cs
+++ src/Chirp.DBFacade/DBFacade.cs
@@ -12,8 +12,8 @@ public class DBFacade<T> : IDataBaseRepository<T> {
         SQL.raw.SetProvider(new SQL.SQLite3Provider_sqlite3());
     }
 
-    private readonly Type _type = typeof(T);
-    private readonly string _name = typeof(T).Name;
+    private static readonly Type Type = typeof(T);
+    private static readonly string Name = typeof(T).Name;
     private const string DEFAULT_DATABASE_FILENAME = "Chirp.db";
     private const string SCHEMA_PATH = "data/schema.sql";
     private const string INITIAL_DATA_PATH = "data/dump.sql";
@@ -22,20 +22,24 @@ public class DBFacade<T> : IDataBaseRepository<T> {
         Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? "";
 
     /** Return [username, text, pub_date] */
-    private const string READ_QUERY =
+    private const string ReadQuery =
         "SELECT username, text, pub_date FROM message JOIN user ON author_id=user_id ORDER BY pub_date desc";
 
     /** Returns the user_id, and count thereof, which matches whatever @username is replaced with.
      * In other words, even if no exceptions were to be thrown from this, we could still check the second field
      * to know whether the person exists. */
-    private const string CHECK_USERNAME_QUERY =
-        "SELECT DISTINCT (user_id, COUNT(user_id)) FROM user JOIN message ON author_id=user_id WHERE username=@username;";
+    private static string Check_Username_Query(string value) =>
+        $"SELECT DISTINCT (user_id, COUNT(user_id)) FROM user JOIN message ON author_id=user_id WHERE username={value}";
 
     /** Insert the new record into the message table, supplying the 'author_id' field from the
      user table by selecting this as one of the values. If something goes wrong, the operation is
      aborted and any changes in progress are automatically rolled back. */
-    private const string INSERT =
-        "INSERT OR ROLLBACK INTO message (author_id, text, pub_date) VALUES ((SELECT DISTINCT user_id FROM user JOIN message ON author_id=user_id WHERE username=@username), @text, @pub_date)";
+    private static string PrepareInsertionCommand(string[] values) =>
+            "INSERT OR ROLLBACK INTO message (author_id, text, pub_date) " +
+            "VALUES (" +
+            "(SELECT DISTINCT user_id " +
+            "FROM user JOIN message ON author_id=user_id " +
+            $"WHERE username={values[0]}), {values[1]}, {values[2]})";
 
     private static DBFacade<T>? _instance = null; //  ? makes it nullable
     private static readonly Lock _padlock = new();
@@ -82,12 +86,11 @@ public class DBFacade<T> : IDataBaseRepository<T> {
     }
 
     /// Returns the correct query based on the input <tt>limit</tt>.
-    private static string DetermineQueryString(int? limit = null) {
-        if (limit.HasValue) {
-            return string.Concat(READ_QUERY, " LIMIT ", limit.ToString());
-        }
-
-        return READ_QUERY;
+    /// Expects <tt>limit</tt> to be positive.
+    private static string PrepareReadQuery(int? limit = null) {
+        if (limit.HasValue)
+            return string.Concat(ReadQuery, " LIMIT ", limit.ToString());
+        return ReadQuery;
     }
 
     /** <inheritdoc cref="IDataBaseRepository{T}.Read(int?)"/>
@@ -99,36 +102,33 @@ public class DBFacade<T> : IDataBaseRepository<T> {
         using SqliteConnection connection = Connect();
         connection.Open();
         using SqliteCommand command = connection.CreateCommand();
-        command.CommandText = DetermineQueryString(limit);
+        command.CommandText = PrepareReadQuery(limit);
         using SqliteDataReader reader = command.ExecuteReader();
 
         if (!reader.HasRows) return [];
         if (reader.IsClosed) throw new SqliteException("The database reader is closed", 1);
-        if (reader.FieldCount != _type.GetProperties().Length) { // Won't be able to convert
+        if (reader.FieldCount != Type.GetProperties().Length) { // Won't be able to convert
             throw new InvalidCastException("The number of columns returned (" + reader.FieldCount +
                                            ") does not match the number of properties in " +
-                                           _name + " (" + _type.GetProperties().Length + ")");
+                                           Name + " (" + Type.GetProperties().Length + ")");
         }
 
         // Get the types of T's properties, and then the matching constructor method.
-        Type[] types = _type.GetProperties().Select(p => p.PropertyType).ToArray();
-        ConstructorInfo? constructor = _type.GetConstructor(types);
+        Type[] types = Type.GetProperties().Select(p => p.PropertyType).ToArray();
+        ConstructorInfo? constructor = Type.GetConstructor(types);
         if (constructor == null) {
-            string typeString = "[" +
-                                string.Join(", ", types.Select(p => p.Name)) +
-                                "]";
-            throw new MissingMethodException("Could not find a constructor for " + _name
-                + " which matches the property types " + typeString);
+            throw new MissingMethodException("Could not find a constructor for " + Name
+                + " which matches its property types.");
         }
 
-        var values = new object?[reader.FieldCount]; // Array in which to store a row from the database.
+        var values = new object?[reader.FieldCount];
         var data = new List<T>();
         while (reader.Read()) {
             reader.GetValues(values); // stores current row in 'values'
             object? record = constructor.Invoke(values); // call constructor with params from 'values'
             if (record == null) {
                 throw new NullReferenceException(
-                    "Record is null; something went wrong while invoking the constructor of " + _name);
+                    "Record is null; something went wrong while invoking the constructor of " + Name);
             }
 
             data.Add((T)record);
@@ -139,43 +139,55 @@ public class DBFacade<T> : IDataBaseRepository<T> {
         return data;
     }
 
+    /** SQL escapes ' by putting another one before. */
+    private static string EscapeQuotes(string value) => 
+        value.Replace("'", "\'\'");
+    
+    /** We add single-quotes around the string after making sure all apostrophes are
+     * properly escaped. */
+    private static string SanitiseString(string value) => 
+        '\'' + EscapeQuotes(value) + '\'';
+    
+    /** Make sure everything is non-null and can be formatted into a string.
+     */
+    private static string[] PrepareValues(T record) {
+        PropertyInfo[] properties = Type.GetProperties();
+        Type stringType = typeof(string);
+        var values = new string[properties.Length];
+        for (var i = 0; i < properties.Length; i++) {
+            PropertyInfo p = properties[i];
+            object v = p.GetValue(record) ?? 
+                       throw new NullReferenceException("Value of " + p.Name + " is null.");
+            string s = v.ToString() ??
+                       throw new NullReferenceException("Value of " + p.Name + 
+                                                        " could not be converted to a string.");
+            if (p.PropertyType == stringType) {
+                values[i] = SanitiseString(s);
+            } else {
+                values[i] = EscapeQuotes(s);
+            }
+        }
+
+        return values;
+    }
+
     public void Store(T record) {
         using SqliteConnection connection = Connect();
         connection.Open();
         using SqliteCommand command = connection.CreateCommand();
-
-        PropertyInfo[] props = _type.GetProperties();
-        if (props.Length != 3) {
-            throw new InvalidCastException("Trying to insert record with " + props.Length + " values.");
-        }
-
-        for (int i = 0; i < props.Length; i++) {
-            if (props[i] == null) {
-                throw new NullReferenceException("Property #" + i + " of new record is null");
-            }
-
-            object? val = props[i].GetValue(record, null);
-            if (val == null) {
-                throw new NullReferenceException("Value of " + props[i].Name + " is null.");
-            }
-        }
-
-        string cmd = INSERT.Replace("@username", props[0].GetValue(record)!.ToString());
-        cmd = cmd.Replace("@text", props[1].GetValue(record)!.ToString());
-        cmd = cmd.Replace("@pub_date", props[2].GetValue(record)!.ToString());
-        command.CommandText = cmd;
+        string[] values = PrepareValues(record);
+        command.CommandText = PrepareInsertionCommand(values);
         command.Transaction = connection.BeginTransaction();
-        int updatedRows = -2;
+        int updatedRows;
         try {
             updatedRows = command.ExecuteNonQuery();
         } catch {
             command.Transaction.Rollback();
             connection.Close();
-            Console.Error.WriteLine("An SQLite error occured during execution of the command." +
+            Console.Error.WriteLine("A(n) SQLite error occured during execution of the command." +
                                     "The transaction has been rolled back.");
             throw;
         }
-
         switch (updatedRows) {
             case 0:
                 command.Transaction.Rollback();
@@ -185,6 +197,7 @@ public class DBFacade<T> : IDataBaseRepository<T> {
                 command.Transaction.Rollback();
                 connection.Close();
                 throw new Exception("More than one row was updated during execution of the command.");
+            // The only case where the transaction was successful is when precisely one row was added.
             case 1:
                 command.Transaction.Commit();
                 connection.Close();
