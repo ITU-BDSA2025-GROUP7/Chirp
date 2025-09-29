9b70beb24cd9432cfa4b74ea957c60c382106042
Louis <louis@falkknudsen.com>
2025-09-29 12:38:05 +0200

    Moved the DBFacade test cases for reading from the full data dump to its own file so that we can get away with only setting the environment variables once per file, rather than also doing it inside the test function as well.

---
+++ tests/Chirp.DBFacade.Tests/ReadTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Chirp.CSVDB;
+using Chirp.General;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Chirp.DBFacade.Tests;
+
+[TestSubject(typeof(DBFacade<>))]
+public class ReadTests {
+    public ReadTests() {
+        // The file has to be embedded in the project settings, see Chirp.DBFacade.Tests.csproj.
+        Environment.SetEnvironmentVariable(DBEnv.envCHIRPDBPATH, Guid.NewGuid().ToString("N") + ".db");
+        Environment.SetEnvironmentVariable(DBEnv.envSCHEMA, "data/schema.sql");
+        Environment.SetEnvironmentVariable(DBEnv.envDATA, "data/dump.sql");
+        Console.SetOut(new StringWriter());
+    }
+
+    /** Asserts that you can successfully read different amounts of records.
+     * 657 is the normal amount in the database at time of writing.
+     * Depending on parallelisation shenanigans, however, this may not be the case when this
+     * test is executed. */
+    [Theory]
+    [InlineData(null, 657, 661)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(0, 0)]
+    [InlineData(800, 657, 661)]
+    public void Read(int? limit, int expectedCount, int? alternative = null) {
+        IDataBaseRepository<Cheep> database = DBFacade<Cheep>.Instance;
+        IEnumerable<Cheep> results = database.Read(limit);
+        Assert.NotNull(results);
+        Cheep[] arr = results as Cheep[] ?? results.ToArray();
+        if (alternative != null) {
+            Assert.True(
+                expectedCount == arr.ToList().Count 
+                || alternative == arr.ToList().Count);
+        } else {
+            Assert.Equal(expectedCount, arr.ToList().Count);
+        }
+    }
+}
