7ef505294c60ea42b491308c4cd9b4f920104abf
Nikki <nikki.s.risager@gmail.com>
2025-10-03 12:39:21 +0200

    Made EF Core migration
    Co-Authored-By: kristofferMejbornEliasson <228927903+KristofferMejbornEliasson@users.noreply.github.com>
    Co-Authored-By: PokingPines <118520090+PokingPines@users.noreply.github.com>
    Co-Authored-By: Louis <6441392+lfknudsen@users.noreply.github.com>
    Co-Authored-By: Mette My Gabelgaard <93643480+MetteMy@users.noreply.github.com>

--- src/Chirp.Razor/Domain Model/Author.cs
+++ src/Chirp.Razor/Domain Model/Author.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Razor.Domain_Model;
 
 public class Author {
+    [Key]
+    public int AuthorId { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
 }
--- src/Chirp.Razor/Domain Model/Cheep.cs
+++ src/Chirp.Razor/Domain Model/Cheep.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Razor.Domain_Model;
 
 public class Cheep {
+    [Key]
+    public int CheepId { get; set; }
     public string Text { get; set; }
     public DateTime Timestamp { get; set; }
     public Author Author { get; set; }
