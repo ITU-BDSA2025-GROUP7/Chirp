5f5183ad4349e3e178b69fd4096b82857cf06d25
Nikki <nikki.s.risager@gmail.com>
2025-10-08 16:41:36 +0200

    Deleted DBFacade and yml file again

--- src/Chirp.DBFacade/DBEnv.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Chirp.DBFacade;
-
-public static class DBEnv {
-    public const string envCHIRPDBPATH = "CHIRPDB_PATH";
-    public const string envSCHEMA = "CHIRPDB_SCHEMA";
-    public const string envDATA = "CHIRPDB_DATA";
-}
