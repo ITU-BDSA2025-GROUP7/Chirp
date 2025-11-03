3aa71aa7c2b6aa71e355d6e5a3b0096b823dabf1
Nikki <nikki.s.risager@gmail.com>
2025-11-03 11:14:15 +0100

    First setup

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -2,48 +2,55 @@ using Chirp.Core;
 using Chirp.Core.Domain_Model;
 using Chirp.Infastructure;
 using Microsoft.EntityFrameworkCore;
+namespace Chirp.Razor;
 
-var builder = WebApplication.CreateBuilder(args);
+public class ChirpProgram
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+        builder.Services.AddRazorPages();
 
-string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.Web.json")
-    .AddJsonFile($"appsettings.Web.{environment}.json", optional:true)
-    .Build();
+        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Web.json")
+            .AddJsonFile($"appsettings.Web.{environment}.json", optional:true)
+            .Build();
 
-string? connectionString = config["ConnectionStrings:DefaultConnection"];
-builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
-builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-builder.Services.AddScoped<ICheepService, CheepService>();
-builder.Services.AddDefaultIdentity<Author>( options =>
-    options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ChirpDBContext>();
+        string? connectionString = config["ConnectionStrings:DefaultConnection"];
+        builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
+        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+        builder.Services.AddScoped<ICheepService, CheepService>();
+        builder.Services.AddDefaultIdentity<Author>( options =>
+            options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ChirpDBContext>();
 
-var app = builder.Build();
+        var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        using (var scope = app.Services.CreateScope())
+        {  /* moved the seeding of the db initializer out of
+            the chirpDBContext so it is possible to use a
+            different test database*/
+            var dbContext = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
+            DbInitializer.SeedDatabase(dbContext);
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-using (var scope = app.Services.CreateScope())
-{  /* moved the seeding of the db initializer out of 
-	the chirpDBContext so it is possible to use a 
-	different test database*/
-    var dbContext = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
-    DbInitializer.SeedDatabase(dbContext);
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapRazorPages();
-
-app.Run();
---
+++ tests/PlaywrightTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using Chirp.Razor;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PlaywrightTests;
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    private WebApplicationFactory<ChirpProgram> _factory;
+    public HttpClient _client;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new WebApplicationFactory<ChirpProgram>();
+        _client = _factory.CreateClient();
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        _factory.Dispose();
+        _client.Dispose();
+    }
+    
+    [Test]
+    public async Task MyTest()
+    {
+        await Page.GotoAsync("http://localhost:5273/");
+        await Expect(Page.Locator("body")).ToContainTextAsync("Public Timeline");
+        await Expect(Page.Locator("span")).ToContainTextAsync("Register");
+        await Expect(Page.Locator("span")).ToContainTextAsync("Login");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Expect(Page.Locator("#account")).ToContainTextAsync("Email");
+        await Expect(Page.Locator("#account")).ToContainTextAsync("Password");
+        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("Nikki@NotAMail.com");
+        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).FillAsync("test");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Listitem)).ToContainTextAsync("Invalid login attempt.");
+    }
+}
