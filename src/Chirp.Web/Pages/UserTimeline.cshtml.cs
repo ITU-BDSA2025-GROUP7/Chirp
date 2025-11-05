accf2d09a57fae6909b227373a8ab5df4af610b5
Louis <louis@falkknudsen.com>
2025-11-05 20:50:23 +0100

    Updated UserTimeline to show an error message to user if the given author does not exist. Otherwise, shows the author's display name before their list of cheeps.

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,24 +1,39 @@
-﻿using System.Security.Claims;
-using System.Security.Principal;
+﻿using Chirp.Core.Domain_Model;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel : CheepTimelineModel
-{
-    public UserTimelineModel(ICheepService service) : base(service)
-    {
+public class UserTimelineModel : CheepTimelineModel {
+    private readonly UserManager<Author> _userManager;
+
+    public UserTimelineModel(ICheepService service, UserManager<Author> userManager)
+        : base(service) {
+        _userManager = userManager;
     }
-    
-    public async Task<IActionResult> OnGet([FromRoute] string author)
-    {
-        int  pageNr = getPageNr(Request);
-        
-        Console.WriteLine("pageQuery: " + pageNr + " author: " + author);
-        
-        Cheeps = await _service.GetCheepsFromAuthor(author, pageNr);
-        
+    public Author? Author { get; set; }
+    public string Header { get; set; } = NO_USER_HEADER;
+
+    private const string NO_USER_HEADER = "User not found";
+
+    public async Task<IActionResult> OnGet([FromRoute] string author) {
+        Author = await _userManager.FindByNameAsync(author);
+        if (Author == null) {
+            Header = NO_USER_HEADER;
+            Cheeps = [];
+        } else {
+            int pageNr = getPageNr(Request);
+            Console.WriteLine("pageQuery: " + pageNr + " author: " + author);
+            Header = FormatPageHeader(Author);
+            Cheeps = await _service.GetCheepsFromAuthor(Author, pageNr);
+        }
         return Page();
+    }
+
+    private static string FormatPageHeader(Author author) {
+        if (author.Name.EndsWith('s')) {
+            return $"{author.Name}' Timeline";
+        }
+        return $"{author.Name}'s Timeline";
     }
 }
