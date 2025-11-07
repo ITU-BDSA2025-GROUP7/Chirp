3006d39a16841bd5da8dc21c43aa7d226b0794b9
Louis <louis@falkknudsen.com>
2025-11-07 16:55:26 +0100

    Further rollback of formatting changes.

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -4,19 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel : CheepTimelineModel {
+public class UserTimelineModel : CheepTimelineModel
+{
+    private const string NO_USER_HEADER = "User not found";
     private readonly UserManager<Author> _userManager;
-
-    public UserTimelineModel(ICheepService service, UserManager<Author> userManager)
-        : base(service) {
-        _userManager = userManager;
-    }
     public Author? Author { get; set; }
     public string Header { get; set; } = NO_USER_HEADER;
 
-    private const string NO_USER_HEADER = "User not found";
+    public UserTimelineModel(ICheepService service, UserManager<Author> userManager)
+        : base(service)
+    {
+        _userManager = userManager;
+    }
 
-    public async Task<IActionResult> OnGet([FromRoute] string author) {
+    public async Task<IActionResult> OnGet([FromRoute] string author)
+    {
         Author = await _userManager.FindByNameAsync(author);
         if (Author == null) {
             Header = NO_USER_HEADER;
@@ -30,7 +32,8 @@ public class UserTimelineModel : CheepTimelineModel {
         return Page();
     }
 
-    private static string FormatPageHeader(Author author) {
+    private static string FormatPageHeader(Author author)
+    {
         if (author.DisplayName.EndsWith('s')) {
             return $"{author.DisplayName}' Timeline";
         }
