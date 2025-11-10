eb2d475aad1af096f368acc25fc79ca60c65bb46
PokingPines <hassan069590@gmail.com>
2025-11-10 20:13:52 +0100

    debug azure secrets

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+Console.WriteLine(builder.Configuration["Authentication.GitHub.ClientId"]);
+Console.WriteLine(builder.Configuration["Authentication.GitHub.ClientSecret"]);
 
 string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
 var config = new ConfigurationBuilder()
