2d95436a0494db8cf82ed93b71f0c325634d78ae
My <mmga@itu.dk>
2025-11-13 16:59:09 +0100

    cleanup

--- src/Chirp.Web/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -46,7 +46,6 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
             StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
-            // StatusMessage = "Thank you for confirming your email. i wrote this";
             return Page();
         }
     }
