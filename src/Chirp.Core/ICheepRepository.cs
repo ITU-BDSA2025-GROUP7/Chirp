40c1fe34339ae3f59833ffd699793e5555340463
Louis <louis@falkknudsen.com>
2025-11-30 19:58:28 +0100

    Expand `ICheepService` and `ICheepRepository` to include methods for retrieving the number of cheeps written by a given author, or a given author's followed accounts. They also expose a property for the total number of cheeps in the database.

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -4,7 +4,7 @@ using Domain_Model;
 
 public interface ICheepRepository {
     /// The number of cheeps to display on each page.
-    const int CHEEPS_PER_PAGE = 32;
+    public const int CHEEPS_PER_PAGE = 32;
 
     /**
      * Gets all cheeps within the given page nr
@@ -16,9 +16,21 @@ public interface ICheepRepository {
      */
     public Task<List<CheepDTO>> GetCheepsFromUserName(string username, int pageNr);
 
+    /** Retrieves the given page of cheeps written by <c>username</c>. */
     public Task<List<CheepDTO>> GetAllCheepsFromUserName(string username);
 
-    public Task<List<CheepDTO>> GetOwnAndFollowedCheeps(Author author, int pageNr = 1);
+    /** Computes the number of cheeps written by <c>username</c>. */
+    public Task<int> CheepCountFromUserName(string username);
 
+    /** Retrieves the given page of cheeps written by all of <c>username</c>'s followers. */
+    public Task<List<CheepDTO>> GetCheepsFromFollowed(Author author, int pageNr = 1);
+
+    /** Computes the number of cheeps written by all <c>username</c>'s followed accounts. */
+    public Task<int> CheepCountFromFollowed(string username);
+
+    /** Creates a new cheep by <c>author</c> with the given <c>message</c> content. */
     public Task CreateCheep(Author author, string message, DateTime timestamp);
+
+    /** The total number of cheeps in the database. */
+    int TotalCheepCount { get; }
 }
