efec5fde484e8f74eb74bf80eb2f18e33509dc20
Louis <louis@falkknudsen.com>
2025-12-02 01:44:30 +0100

    Forgot to commit line that had to be changed in old test.

--- tests/PlaywrightTests/PlayWrightTests.cs
+++ tests/PlaywrightTests/PlayWrightTests.cs
@@ -111,7 +111,7 @@ public class PlayWrightTests : PageTest, IClassFixture<EndToEndWebApplicationFac
         await Expect(Page.Locator("body")).ToBeVisibleAsync();
         await Expect(Page.Locator("h2")).ToContainTextAsync("Public Timeline");
         await Expect(Page.Locator("body"))
-           .ToContainTextAsync("My Timeline | Public Timeline | About me | Logout");
+           .ToContainTextAsync("My Timeline | Public Timeline | Search | About me | Logout");
         await Expect(Page.Locator("body")).ToContainTextAsync("What's on your mind, Helge? Share");
         await Expect(Page.Locator("h1")).ToContainTextAsync("Chirp!");
     }
